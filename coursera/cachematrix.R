b72d03d9922febe95204500392ef8c5c4efebb02
Lorraine Chong <lorr.chong@gmail.com>
2015-12-24 13:45:07 -0500

    Coursera: R Programming Assignment 2 - Caching the Inverse of a Matrix

---
+++ coursera/cachematrix.R
@@ -0,0 +1,35 @@
+# R Programming Assignment 2: Caching the Inverse of a Matrix
+#
+# Matrix inversion is usually a costly computation and there may be some benefit
+# to caching the inverse of a matrix rather than compute it repeatedly. 
+#
+# 1. makeCacheMatrix creates a special "matrix" object that can cache its inverse.
+#
+# 2. cacheSolve computes the inverse of the "matrix" returned by makeCacheMatrix.
+#    If the inverse has already been calculated (and the matrix has not changed),
+#    then cachesolve should retrieve the inverse from the cache.
+#
+makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv 
+        list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+}
+
+cacheSolve <- function(x, ...) {
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
+}
+
